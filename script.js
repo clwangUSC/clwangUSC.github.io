ba3f7441431feb5dd6cf9caabd79d4d386d8dffa
clwangUSC <clwang@usc.edu>
2023-05-26 18:48:29 +0800

    hello world

---
+++ script.js
@@ -0,0 +1,11 @@
+function goHome () {
+    document.getElementById("theSprite").src = "images/Home.png";
+}
+
+function goPort () {
+    document.getElementById("theSprite").src = "images/Portfolio.png";
+}
+
+function goMe () {
+    document.getElementById("theSprite").src = "images/AboutMe.png";
+}
