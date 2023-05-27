b91d633515fb23629f1e09b9ff5a45a803fe81ef
clwangUSC <clwang@usc.edu>
2023-05-27 10:42:24 +0800

    added scroll for port

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function goHome () {
-    document.getElementById("theSprite").src = "images/Home.png";
-}
-
-function goPort () {
-    document.getElementById("theSprite").src = "images/Portfolio.png";
-}
-
-function goMe () {
-    document.getElementById("theSprite").src = "images/AboutMe.png";
+function toggleP(button) {
+    var portfolio = document.getElementById("portfolio");
+    if(button.id==="goHome")
+    {
+        document.getElementById("theSprite").src = "images/Home.png";
+        portfolio.style.display="none";
+    }
+    else if(button.id==="goPort")
+    {
+        document.getElementById("theSprite").src = "images/Portfolio.png";
+        portfolio.style.display="block";
+    }
+    else
+    {
+        document.getElementById("theSprite").src = "images/AboutMe.png";
+        portfolio.style.display="none";
+    }
 }
